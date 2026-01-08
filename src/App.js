d622c6446781d45cc44b6e34b3855e957a4ac8fc
윤태영 <yoonty89@gmail.com>
2026-01-08 14:34:18 +0900

    Auto deploy on 2026-01-08 14:34:18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import ImageAnalysis from "./pages/ImageAnalysis/ImageAnalysis";
-import KeywordSearch from "./pages/KeywordSearch/KeywordSearch";
+import ImageAnalysisPage from "./pages/ImageAnalysis/ImageAnalysisPage";
+import KeywordSearchPage from "./pages/KeywordSearch/KeywordSearchPage";
 import "./styles/App.css";
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
         {/* URL에 따라 내용이 바뀌는 부분 */}
         <div className="content">
           <Routes>
-            <Route path="/" element={<ImageAnalysis />} />
-            <Route path="/search" element={<KeywordSearch />} />
+            <Route path="/" element={<ImageAnalysisPage />} />
+            <Route path="/search" element={<KeywordSearchPage />} />
           </Routes>
         </div>
       </div>
